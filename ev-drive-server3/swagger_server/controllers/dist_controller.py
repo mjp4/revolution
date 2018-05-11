df37f33124a534dce63f8a48da3d43ee2cd17963
0990linian <1445060616@qq.com>
2018-05-11 14:03:57 +0100

    Add extra time Function

--- ev-drive-server3/swagger_server/controllers/dist_controller.py
+++ ev-drive-server3/swagger_server/controllers/dist_controller.py
@@ -50,12 +50,31 @@ def get_dist_to_charger(lat_here, long_here, lat_there, long_there):  # noqa: E5
     #     * (1 - cos((long_there - long_here) * p)) / 2
     # return 12742 * asin(sqrt(a))
 
-def extra_time_to_charger(lat_here, long_here, lat_there, long_there):
+def duration_to_time(duration_str):
+    duration_list = duration_str.split(" ")
+    if len(duration_list) > 2:
+        time = int(duration_list[0]) * 60 + int(duration_list[2])
+    elif len(duration_list) == 2:
+        time = int(duration_list[0])
+
+    return time
+
+def extra_time_to_charger(lat_here, long_here, lat_charger, long_chager, lat_dest, lon_dest):
+
+    api_key = "AIzaSyCsYddGN_7QHkR-JnCS4PA-giJV79ZqW7c"
 
 
-
-    return "10 minutes"
-
-
-
-
+    url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins={},{}|{},{}&destinations={},{}|{},{}&key={}" \
+        .format(lat_here, long_here, lat_charger, long_chager, lat_charger, long_chager, lat_dest, lon_dest, api_key)
+    
+    print(url)
+    website_text_result = requests.get(url).text
+    data = json.loads(website_text_result)
+    
+    origin_dest_time = data['rows'][0]['elements'][0]['duration']['text']
+    origin_charger_time = data['rows'][0]['elements'][1]['duration']['text']
+    charger_dest_time = data['rows'][1]['elements'][1]['duration']['text']
+    
+    extra_time = duration_to_time(origin_charger_time) + duration_to_time(charger_dest_time) - duration_to_time(origin_dest_time)
+    
+    return extra_time
--- ev-drive-server3/swagger_server/models/test.py
+++ ev-drive-server3/swagger_server/models/test.py
@@ -1,30 +1,35 @@
-import connexion
+# import connexion
 import six
 import requests
 import json
 import wget
 
-def get_time_to_charger(lat_here, long_here, lat_charger, long_chager, lat_dest, lon_dest):
+
+def duration_to_time(duration_str):
+    duration_list = duration_str.split(" ")
+    if len(duration_list) > 2:
+        time = int(duration_list[0]) * 60 + int(duration_list[2])
+    elif len(duration_list) == 2:
+        time = int(duration_list[0])
+
+    return time
+
+def extra_time_to_charger(lat_here, long_here, lat_charger, long_chager, lat_dest, lon_dest):
 
     api_key = "AIzaSyCsYddGN_7QHkR-JnCS4PA-giJV79ZqW7c"
 
 
     url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins={},{}|{},{}&destinations={},{}|{},{}&key={}" \
         .format(lat_here, long_here, lat_charger, long_chager, lat_charger, long_chager, lat_dest, lon_dest, api_key)
-    print(url)
-    fs = wget.download(url=url, out='dist.json')
-    with open(fs, 'r') as f:
-        content = f.read()
-
-    data = json.loads(content)
     
-    original_time = data['rows'][0]['elements'][0]['duration']['text']
-
-    print(original_time)
-
-
-    #miles = data['rows'][0]['elements'][0]['distance']['text']
-
-    #return miles[:-3]
-
-get_time_to_charger(55.838968, -4.263472, 55.504997, -4.368097,55.631621, -3.872165)
+    print(url)
+    website_text_result = requests.get(url).text
+    data = json.loads(website_text_result)
+    
+    origin_dest_time = data['rows'][0]['elements'][0]['duration']['text']
+    origin_charger_time = data['rows'][0]['elements'][1]['duration']['text']
+    charger_dest_time = data['rows'][1]['elements'][1]['duration']['text']
+    
+    extra_time = duration_to_time(origin_charger_time) + duration_to_time(charger_dest_time) - duration_to_time(origin_dest_time)
+    
+    return extra_time
