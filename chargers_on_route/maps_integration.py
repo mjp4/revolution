c0210ffa6ae1dd70f22e4f7615b18cdb64afa624
DCL\DR5 <david.rodriguez@metaswitch.com>
2018-05-11 09:39:22 +0100

    Translation function between postcode and coordinates

--- chargers_on_route/maps_integration.py
+++ chargers_on_route/maps_integration.py
@@ -28,3 +28,23 @@ def get_route(start, end):
         new_coordinate_list.append(point)
     
     return new_coordinate_list
+
+def get_coordinates(name):
+    """
+    This function returns the latitude and longitude for a given postcode
+    
+    Input the postcode, as a string
+    Returns a list with two elements, latitude and longitude
+    """
+
+    base_url = "http://api.postcodes.io/postcodes/{0}"
+    get_url = base_url.format(name)
+    website_text_result = requests.get(get_url).text
+    website_result = json.loads(website_text_result)
+
+    if website_result['status'] != 200:
+        return []
+    latitude = website_result['result']['latitude']
+    longitude = website_result['result']['longitude']
+   
+    return [latitude,longitude]
