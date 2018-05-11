0bcbc072556267b43cb2cb063785132da5e8b6f5
bjt <ben.trigg@metaswitch.com>
2018-05-11 11:56:49 +0100

    Reverting all_controller.py

--- ev-drive-server3/swagger_server/controllers/all_controller.py
+++ ev-drive-server3/swagger_server/controllers/all_controller.py
@@ -1,14 +1,10 @@
 import connexion
 import six
-import logging
 from swagger_server.controllers import status_controller
+
 from swagger_server.models.all_info import AllInfo  # noqa: E501
 from swagger_server import util
-import status_controller
-import dist_controller
 
-
-logging.debug("login = %s , password = %s" % ( username , password)  )
 
 def all_info(username, password, to):  # noqa: E501
     """Get all the info
@@ -24,25 +20,8 @@ def all_info(username, password, to):  # noqa: E501
 
     :rtype: AllInfo
     """
-    ###############CONVERT 'to' to long and lat ####################
 
-    # Log in
-    l = login(username, password)
-
-    # return the car status, and the list of charge points
     a = status_controller.get_charge_perc(username, password)
 
-    get chargers()
 
     return a
-
-
-def get_chargers(car_lat, car_lon ):
-    # Filter chargers on route. (Nian)
-    chargers = []
-
-    # Calculate distances to charger. Order by nearest
-    for charger in chargers:
-        charg_lat = charger.get("lat")
-        charg_lon = charger.get("lon")
-        get_dist_to_charger(lat_here, long_here, lat_there, long_there):
