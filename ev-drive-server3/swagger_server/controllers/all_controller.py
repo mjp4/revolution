36e7ae89fe5ce8b24b31315abc8a5ba47ac47c56
Mark Perryman <mark.perryman@metaswitch.com>
2018-05-11 16:35:46 +0100

    .

--- ev-drive-server3/swagger_server/controllers/all_controller.py
+++ ev-drive-server3/swagger_server/controllers/all_controller.py
@@ -23,9 +23,9 @@ def all_info(username, password, to):  # noqa: E501
     :type to: str
     :rtype: AllInfo
     """
-    # car_status = status_controller.get_charge_perc(username, password)
-
     username = "mark@perryman.org.uk"
+
+    # car_status = status_controller.get_charge_perc(username, password)
 
     car_status = { "watthour": "18160",
                    "location_lat": "52.375666666667",
